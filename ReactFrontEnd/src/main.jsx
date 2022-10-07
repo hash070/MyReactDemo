d384da2943b7fa0475b8af1d978869e6b6b7da64
hash070 <hash070@outlook.com>
2022-10-07 13:02:48 +0800

    Refactor Front Project

--- ReactFrontEnd/src/main.jsx
+++ ReactFrontEnd/src/main.jsx
@@ -1,118 +1,19 @@
-import React, {Component} from 'react'
-import ReactDOM from 'react-dom/client'
-import {BrowserRouter, createBrowserRouter, RouterProvider} from "react-router-dom";
-import {Button, DatePicker} from 'antd';
-import 'antd/dist/antd.css';
-import TodoList from "./toys/TodoList.jsx";
+import React, {Component} from 'react';
+import ReactDOM from "react-dom/client";
 
-import ConditionRenderTest from "./toys/ConditionRenderTest.jsx";
-import PropAndStatus from "./toys/PropAndStatus.jsx";
-import ControlledInput from "./toys/ControlledInput.jsx";
-import HooksTest from "./toys/HooksTest.jsx";
-import TodoListFunc from "./toys/TodoListFunc.jsx";
-import MyRouter from "./routers/MyRouter.jsx";
-import MyRouterWithFilter from "./toys/RouterTest2/MyRouterWithFilter.jsx";
-import NotFound from "./toys/RouterTest2/NotFound.jsx"; // or 'antd/dist/antd.less'
-// import App from './App.jsx'
-
-class AppTest extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            date: new Date(),
-            textx: "Hello, world!",
-            listTest: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        };
-    }
-
-
+class Main extends Component {
     render() {
         return (
             <div>
-                <p onClick={() => {
-                    this.setState({textx: "Hello, world!2222"})
-                }}>{this.state.textx}</p>
-
-                <ul>
-                    {
-                        //遍历数组
-                        this.state.listTest.map((item, index) => (
-                            <li key={index}>{item}</li>
-                        ))
-                    }
-                </ul>
-            </div>
-        );
-    }
-}
-class Root extends Component {
-    render() {
-        return (
-            <div>
-                Root
+                HelloWorld
             </div>
         );
     }
 }
 
-class Dashboard extends Component {
-    render() {
-        return (
-            <div>
-                Dashboard
-            </div>
-        );
-    }
-}
-
-class About extends Component {
-    render() {
-        return (
-            <div>
-                About
-            </div>
-        );
-    }
-}
-
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Root />,
-        children: [
-            {
-                path: "dashboard",
-                element: <Dashboard />,
-            },
-            {
-                path: "about",
-                element: <About />,
-            },
-        ],
-    },
-]);
-
-class BigSpinner extends Component {
-    render() {
-        return (
-            <div>
-                BigSpinner
-            </div>
-        );
-    }
-}
-/*
-            <RouterProvider
-                router={router}
-                fallbackElement={<BigSpinner />}
-                errorElement={<NotFound/>}
-            />
-* */
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+let root= ReactDOM.createRoot(document.getElementById('root'))
+root.render(
     <div>
-        <React.StrictMode>
-            <MyRouterWithFilter/>
-        </React.StrictMode>
+        <Main/>
     </div>
 )
