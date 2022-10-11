720d0e38cd6d8768a2d09bd703c95bf775b28ce9
hash070 <hash070@outlook.com>
2022-10-11 10:55:21 +0800

    Allow all CORS

--- SpringBootBackEnd/src/main/java/top/hash070/reactdemobackend/reactdemobackend/ReactDemoBackEndApplication.java
+++ SpringBootBackEnd/src/main/java/top/hash070/reactdemobackend/reactdemobackend/ReactDemoBackEndApplication.java
@@ -1,8 +1,8 @@
 package top.hash070.reactdemobackend.reactdemobackend;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +15,8 @@ public class ReactDemoBackEndApplication {
     }
 
     @PostMapping("/login")
+    //CORS allow all
+    @CrossOrigin(origins = "*")
     public User login(@RequestBody User user) {
         System.out.println("用户登录");
         System.out.println("username:" + user.getUsername());
